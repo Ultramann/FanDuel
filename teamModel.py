5666ebeb15f3e59a07434a087575e13b5293b474
Ultramann <cary.goltermann@gmail.com>
2014-12-09 20:35:36 -0700

    PlayerDictionary and Player classes now working

--- teamModel.py
+++ teamModel.py
@@ -1,29 +1,18 @@
 class PlayerDictionary():
-    self.QB = {}
-    self.WR = {}
-    self.RB = {}
-    self.TE = {}
-    self.K = {}
-    self.D = {}
     def __init__(self, player_dict):
+        positions = ['QB', 'WR', 'RB', 'TE', 'K', 'D']
+        new_player_dict = {position: {} for position in positions}
         for player_name in player_dict:
-            player = Player(player_name, player_dict[player_name][2:]) # I THINK THAT INDEX 1 IS THE POSITION...
-            eval('self.' + player.position + '[player_name] = player') # I THINK THIS WILL WORK, BUT I MIGHT END UP WANTING TO HAVE A DICTIONARY WITH KEY: VALUES - PLAYERNAME: PLAYER(CLASS)
+            player = Player(player_name, player_dict[player_name])
+            new_player_dict[player.position][player_name] = player
+        for position, player in new_player_dict.items():
+            setattr(self, position, player)
 
 class Player():
-    def __init__(self, player_name, player_data):
+    def __init__(self, player_name, player_data_dict):
         self.name = player_name
-        for i, attribute in enumerate(['position', 'salary', 'rating', 'rating_mutation',
-                                        'volatility', 'fppg', 'games_played']):
-            setattr(self, attribute, player_data[i])
+        for attribute, data in player_data_dict.items():
+            setattr(self, attribute.lower(), data) 
             
-        self.position = player_data[0]
-        self.salary = player_data[1]
-        self.rating = player_data[2]
-        self.rating_mutation = player_data[3]
-        self.volatility = player_data[4]
-        self.fppg = player_data[5]
-        self.games_played = player_data[6]
-
-class Team(PlayerDictionary):
-    def __init__(self)
+#class Team(PlayerDictionary):
+#    def __init__(self)
